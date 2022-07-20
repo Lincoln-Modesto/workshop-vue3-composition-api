29d127a89363bd5462553c1add03fc50c6768d55
lincoln <lincoln-md@hotmail.com>
2022-07-20 12:46:40 -0300

    exercise 2 and 3

--- src/components/CharacterCards.vue
+++ src/components/CharacterCards.vue
@@ -1,3 +1,47 @@
+<script setup>
+import axios from "axios";
+import { ref } from 'vue'
+
+const characters = ref([])
+const loadingState = ref(null)
+
+function fetchAllCharacters() {
+  loadingState.value = "loading";
+  axios
+    .get("https://rickandmortyapi.com/api/character")
+    .then((response) => {
+      setTimeout(() => {
+        loadingState.value = "success";
+        characters.value = response.data.results;
+      }, 1000);
+    });
+}
+
+fetchAllCharacters()
+</script>
+
+<script>
+
+import orderBy from "lodash/orderby";
+export default {
+  data() {
+    return {
+      orderKey: "id"
+    }
+  },
+  computed: {
+    charactersOrdered() {
+      return orderBy(this.characters, this.orderKey);
+    },
+  },
+  methods: {
+    setOrderKey(key) {
+      this.orderKey = key;
+    },
+  }
+};
+</script>
+
 <template>
   <div>
     <div class="border-b-2 pb-4 border-gray-300 text-center">
@@ -8,19 +52,10 @@
       <button class="btn bg-orange-500" @click="setOrderKey('id')">Id</button>
     </div>
     <div class="m-auto container flex flex-wrap mt-10">
-      <div
-        v-for="character in charactersOrdered"
-        :key="character.id"
-        class="xl:w-1/5 lg:w-1/4 md:w-1/3 w-1/2 card"
-      >
+      <div v-for="character in charactersOrdered" :key="character.id" class="xl:w-1/5 lg:w-1/4 md:w-1/3 w-1/2 card">
         <div class="card-inner">
           <div class="image">
-            <img
-              :src="character.image"
-              class="bg-gray-200"
-              height="300"
-              width="300"
-            />
+            <img :src="character.image" class="bg-gray-200" height="300" width="300" />
           </div>
           <div class="content text-center mt-5">
             <span class="header text-xl">{{ character.name }}</span>
@@ -40,68 +75,21 @@
   </div>
 </template>
 
-<script>
-import axios from "axios";
-import orderBy from "lodash/orderby";
-import { ref } from 'vue'
-
-export default {
-  setup(){
-    const characters = ref([])
-    const loadingState = ref(null)
-
-    function fetchAllCharacters() {
-      loadingState.value = "loading";
-      axios
-        .get("https://rickandmortyapi.com/api/character")
-        .then((response) => {
-          setTimeout(() => {
-            loadingState.value = "success";
-            characters.value = response.data.results;
-          }, 1000);
-        });
-    }
-
-    return {
-      characters,
-      loadingState,
-      fetchAllCharacters 
-    }
-
-  },
-  data() {
-    return {
-      orderKey: "id"
-    }
-  },
-  computed: {
-    charactersOrdered() {
-      return orderBy(this.characters, this.orderKey);
-    },
-  },
-  methods: {
-    setOrderKey(key) {
-      this.orderKey = key;
-    },
-  },
-  created() {
-    this.fetchAllCharacters()
-  },
-};
-</script>
-
 <style scoped>
 .btn {
   @apply px-6 py-1 text-white rounded;
 }
+
 .card-inner {
   @apply p-4 shadow-md rounded m-2;
 }
+
 .card-inner img {
   @apply rounded;
   max-width: auto;
   display: block;
 }
+
 .loading {
   @apply fixed top-0 left-0 right-0 bottom-0 bg-white flex justify-center items-center;
 }
